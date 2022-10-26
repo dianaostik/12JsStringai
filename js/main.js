9e3febc5a4ba86dce6a55c409e6babeab3f34609
dianaostik <dianastud12@gmail.com>
2022-10-26 11:24:08 +0300

    papildomos

--- js/main.js
+++ js/main.js
@@ -35,12 +35,34 @@ console.log(letter);
 // 5 uzd
 
 console.log("------- 5 uduotis");
-let zvaigz = "An American in Paris ";
-console.log( zvaigz.replace(/a|A/g, "*") );
+let america = "An American in Paris";
+console.log( america.replace(/a|A/g, "*") );
 
 // 6 uzd
 
 console.log("------- 6 uduotis");
+let breakf = "Breakfast at Tiffany's";
+let odys = "2001: A Space Odyssey";
+let life = "It's a Wonderful Life";
 
+console.log( america.replace(/a|e|i|o|u|A|E|I|O|U/g, " ") );
+console.log( breakf.replace(/a|e|i|o|u|A|E|I|O|U/g, " ") );
+console.log( odys.replace(/a|e|i|o|u|A|E|I|O|U/g, " ") );
+console.log( life.replace(/a|e|i|o|u|A|E|I|O|U/g, " ") );
 
+// 7 uzd
 
+console.log("------- 7 uduotis");
+
+let num = " ".repeat( Math.ceil(Math.random() * 10))+(Math.ceil (Math.random() * 7)+1);
+let starWars = "Star Wars: Episode "+ num + " - A New Hope";
+console.log(starWars);
+console.log(num);
+
+// 8 uzd
+
+console.log("------- 8 uduotis");
+ let south = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood";
+ let pietu = "Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale";
+
+ console.log();
